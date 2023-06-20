6203838bde78acbd91809c7f1b70740a7401b408
Louise NAUD <louisenaud@users.noreply.github.com>
2023-06-19 22:41:05 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,8 +3,10 @@ if conda info --envs | grep -q docugami-challenge; then echo "docugami-challenge
 conda activate docugami-challenge
 pip install -r requirements.txt
 # download xml file in created data folder
-mkdir data && cd data
-wget https://github.com/midas-network/COVID-19/raw/master/documents/mendeley_library_files/xml_files/mendeley_document_library_2020-03-25.xml
+mkdir data
+cd data
+wget -c https://github.com/midas-network/COVID-19/raw/master/documents/mendeley_library_files/xml_files/mendeley_document_library_2020-03-25.xml
+# shellcheck disable=SC2103
 cd ..
 # create results directory
 mkdir results
@@ -15,11 +17,43 @@ python -m scripts.run_data_exploration
 # perform preliminary experiments
 python -m scripts.run_preliminary_experiments
 
+mlflow ui
 
-#python src/train.py model=kmeans_sk model.n_clusters=2
-#python main.py model=kmeans_sk hydra.mode=MULTIRUN model.n_clusters=2,3,4,5,6,7
-#python main.py +experiment=kmeans_sklearn hydra.mode=MULTIRUN model.n_clusters=2,3,4,5,6,7
-python main.py model=kmeans_sk model.n_clusters=2
-python main.py model=kmeans_sk model.n_clusters=3
-python main.py model=kmeans_sk model.n_clusters=4
-python main.py model=kmeans_sk preprocessing_pipeline=count
+# kmeans experiments
+python main.py model=kmeans_sk model.n_clusters=2 mlflow.experiment_name=kmeans
+python main.py model=kmeans_sk model.n_clusters=3 mlflow.experiment_name=kmeans
+python main.py model=kmeans_sk model.n_clusters=4 mlflow.experiment_name=kmeans
+python main.py model=kmeans_sk model.n_clusters=2 preprocessing_pipeline=count mlflow.experiment_name=kmeans
+python main.py model=kmeans_sk model.n_clusters=3 preprocessing_pipeline=count mlflow.experiment_name=kmeans
+python main.py model=kmeans_sk model.n_clusters=4 preprocessing_pipeline=count mlflow.experiment_name=kmeans
+python main.py model=kmeans_sk model.n_clusters=2 preprocessing_pipeline=count_tsvd_dim3 mlflow.experiment_name=kmeans
+python main.py model=kmeans_sk model.n_clusters=3 preprocessing_pipeline=count_tsvd_dim3 mlflow.experiment_name=kmeans
+python main.py model=kmeans_sk model.n_clusters=4 preprocessing_pipeline=count_tsvd_dim3 mlflow.experiment_name=kmeans
+
+# spectral clustering experiments
+python main.py model=spectral_nn model.n_clusters=2 mlflow.experiment_name=scnn
+python main.py model=spectral_nn model.n_clusters=3 mlflow.experiment_name=scnn
+python main.py model=spectral_nn model.n_clusters=4 mlflow.experiment_name=scnn
+python main.py model=spectral_nn model.n_clusters=2 preprocessing_pipeline=count mlflow.experiment_name=scnn
+python main.py model=spectral_nn model.n_clusters=3 preprocessing_pipeline=count mlflow.experiment_name=scnn
+python main.py model=spectral_nn model.n_clusters=4 preprocessing_pipeline=count mlflow.experiment_name=scnn
+python main.py model=spectral_nn model.n_clusters=2 preprocessing_pipeline=count_tsvd_dim3 mlflow.experiment_name=scnn
+python main.py model=spectral_nn model.n_clusters=3 preprocessing_pipeline=count_tsvd_dim3 mlflow.experiment_name=scnn
+python main.py model=spectral_nn model.n_clusters=4 preprocessing_pipeline=count_tsvd_dim3 mlflow.experiment_name=scnn
+
+python main.py model=spectral_rbf model.n_clusters=2 mlflow.experiment_name=scrbf
+python main.py model=spectral_rbf model.n_clusters=3 mlflow.experiment_name=scrbf
+python main.py model=spectral_rbf model.n_clusters=4 mlflow.experiment_name=scrbf
+python main.py model=spectral_rbf model.n_clusters=2 preprocessing_pipeline=count mlflow.experiment_name=scrbf
+python main.py model=spectral_rbf model.n_clusters=3 preprocessing_pipeline=count mlflow.experiment_name=scrbf
+python main.py model=spectral_rbf model.n_clusters=4 preprocessing_pipeline=count mlflow.experiment_name=scrbf
+python main.py model=spectral_rbf model.n_clusters=2 preprocessing_pipeline=count_tsvd_dim3 mlflow.experiment_name=scrbf
+python main.py model=spectral_rbf model.n_clusters=3 preprocessing_pipeline=count_tsvd_dim3 mlflow.experiment_name=scrbf
+python main.py model=spectral_rbf model.n_clusters=4 preprocessing_pipeline=count_tsvd_dim3 mlflow.experiment_name=scrbf
+
+# affinity propagation experiments
+python main.py model=affinity_propagation mlflow.experiment_name=scrbf
+python main.py model=affinity_propagation preprocessing_pipeline=count mlflow.experiment_name=affinity_propagation
+python main.py model=affinity_propagation preprocessing_pipeline=count_tsvd_dim3 mlflow.experiment_name=affinity_propagation
+
+
